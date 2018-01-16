8aa7ab4eb7fbb116cce706c077f221a4b350b3f3
antklim <antklim@gmail.com>
2018-01-16 17:11:26 +1100

    3 sum problem solution

--- 3sum.js
+++ 3sum.js
@@ -32,66 +32,42 @@ nzpTestCases.forEach(test => {
     `negatives: ${test.negatives}; positives: ${test.positives}; expected: ${test.expected}; actual: ${actual}`)
 })
 
-const matchPositives = (negative, positives, hasZeros) => {
-  const result = []
-
-  if (hasZeros && positives.includes(Math.abs(negative))) {
-    result.push([0, Math.abs(negative)])
-  }
-
-  const rigthIdx = positives.findIndex(e => e >= Math.abs(Math.floor(negative / 2)))
-
-  for (let i = 0; i <= rigthIdx; i++) {
-    p = positives[i]
-    r = Math.abs(negative + p)
-
-    if (positives.findIndex()) result.push(p, Math.abs(n + p))
-  }
-
-  return result
-}
-
 /**
  * @param  {number[]} negatives a list of negatives
  * @param  {number[]} positives a list of positives
  * @return {number[]}           a list of triples which give zero sum
  */
-const negativePositive = (negatives, positives) => {
+const negativePositivePositive = (negatives, positives) => {
   const result = []
+  const rpositives = [].concat(positives).reverse()
 
   for (let i = 0; i < negatives.length; i++) {
-    const n1 = negatives[i]
+    const n = negatives[i]
+    const half = Math.floor(-1 * n / 2)
 
-    // search for negative, positive, positive
-    const halfPosition = positives.findIndex(el => {
-      el <= Math.abs(Math.floor(n1 / 2))
-    })
+    let halfPosition = rpositives.findIndex(el => el <= half)
 
     if (halfPosition !== -1) {
+      halfPosition = positives.length - 1 - halfPosition
+
+      let previous = null
       for (let j = 0; j <= halfPosition; j++) {
+        if (previous === positives[j]) continue
+        previous = positives[j]
+
         const p1 = positives[j]
-        const p2 = Math.abs(n1 + p1)
+        const p2 = Math.abs(n + p1)
 
-        if (positives.includes(p2)) result.push([n1, p1, p2])
+        if (positives.findIndex((el, idx) => el === p2 && idx > j) !== -1) result.push([n, p1, p2])
       }
-    }
-
-    // search for negative, negative, positive
-    if (i + 1 < negatives.length) {
-      const n2 = negatives[i + 1]
-      const p1 = Math.abs(n1 + n2)
-      if (positives.includes(p1)) result.push([n1, n2, p1])
     }
   }
 
   return result
 }
 
-const npTestCases = [
+const nppTestCases = [
   {negatives: [-1, -1, -2, -7, -9], positives: [1, 2, 3, 4, 5, 7, 9], expected: [
-    [-1, -1, 2],
-    [-1, -2, 3],
-    [-2, -7, 9],
     [-7, 2, 5],
     [-7, 3, 4],
     [-9, 2, 7],
@@ -99,8 +75,53 @@ const npTestCases = [
   ]}
 ]
 
-npTestCases.forEach(test => {
-  const actual = negativePositive(test.negatives, test.positives)
+nppTestCases.forEach(test => {
+  const actual = negativePositivePositive(test.negatives, test.positives)
+  assert.deepEqual(actual, test.expected,
+    `negatives: ${test.negatives}; positives: ${test.positives};\nexpected:\t${JSON.stringify(test.expected)};\nactual:\t\t${JSON.stringify(actual)}`)
+})
+
+/**
+ * @param  {number[]} negatives a list of negatives
+ * @param  {number[]} positives a list of positives
+ * @return {number[]}           a list of triples which give zero sum
+ */
+const negativeNegativePositive = (negatives, positives) => {
+  const result = []
+
+  for (let i = 0; i < positives.length; i++) {
+    const p = positives[i]
+    const half = -1 * Math.floor(p / 2)
+
+    let halfPosition = negatives.findIndex(el => el < half)
+
+    if (halfPosition !== -1) {
+      let previous = null
+      for (let j = 0; j <= halfPosition; j++) {
+        if (previous === negatives[j]) continue
+        previous = negatives[j]
+
+        const n1 = negatives[j]
+        const n2 = -1 * (p + n1)
+
+        if (negatives.findIndex((el, idx) => el === n2 && idx > j) !== -1) result.push([n1, n2, p])
+      }
+    }
+  }
+
+  return result
+}
+
+const nnpTestCases = [
+  {negatives: [-1, -1, -2, -7, -9], positives: [1, 2, 3, 4, 5, 7, 9], expected: [
+    [-1, -1, 2],
+    [-1, -2, 3],
+    [-2, -7, 9],
+  ]}
+]
+
+nnpTestCases.forEach(test => {
+  const actual = negativeNegativePositive(test.negatives, test.positives)
   assert.deepEqual(actual, test.expected,
     `negatives: ${test.negatives}; positives: ${test.positives};\nexpected:\t${JSON.stringify(test.expected)};\nactual:\t\t${JSON.stringify(actual)}`)
 })
@@ -116,7 +137,7 @@ const threeSum = nums => {
   // 3. Negative, Positive, Positive
   // 4. Negative, Negative, Positive
 
-  const result = []
+  let result = []
   const negatives = nums.filter(e => e < 0).sort()
   const zeros = nums.filter(e => e === 0)
   const positives = nums.filter(e => e > 0).sort()
@@ -133,22 +154,20 @@ const threeSum = nums => {
   }
 
   // 3. Negative, Positive, Positive &
+  result = result.concat(negativePositivePositive(negatives, positives))
+
   // 4. Negative, Negative, Positive
-  result = result.concat(negativePositive(negatives, positives))
+  result = result.concat(negativeNegativePositive(negatives, positives))
 
   return result
 }
 
+const threeSumTestCases = [
+  {nums: [-1, 0, 1, 2, -1, -4], expected: [[-1, 0, 1], [-1, -1, 2]]}
+]
 
-// [-9, -6, -4, -2, -1, -1, 0, 0, 0, 1, 2, 4, 5, 6, 7]
-//
-// [
-//   [0, 0, 0]
-//   [-1, 0, 1]
-//   [-4, 0, 4]
-//   [-6, 0, 6]
-//   [-6, 1, 5]
-//   [-9, 4, 5]
-//   [-9, 2, 7]
-//   [6, -4, -2]
-// ]
+threeSumTestCases.forEach(test => {
+  const actual = threeSum(test.nums)
+  assert.deepEqual(actual, test.expected,
+    `nums: ${test.nums};\nexpected:\t${JSON.stringify(test.expected)};\nactual:\t\t${JSON.stringify(actual)}`)
+})
